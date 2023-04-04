3adfee9d0b9694a7fb5d9f7e8e6ccafbdb3203a3
Sequeira12 <brunomsequeira@hotmail.com>
2023-04-05 00:21:46 +0100

    180 RESPEITA ME

--- projeto.cpp
+++ projeto.cpp
@@ -3,8 +3,48 @@
 
 using namespace std;
 int K = 0, R = 0;
+void getTransactionPath(vector<vector<int>> &dp, vector<int> &prices, int index, int buy, vector<int> &path, vector<vector<int>> &paths)
+{
+    if (index >= prices.size())
+    {
+        paths.push_back(path);
 
-int solvetab(vector<int> &prices)
+        return;
+    }
+
+    if (buy)
+    {
+        int BuyKaro = -prices[index] * K + dp[index + 1][0] - K * R;
+        int skipKaro = 0 + dp[index + 1][1];
+        if (BuyKaro >= skipKaro)
+        {
+            path.push_back(index);
+            getTransactionPath(dp, prices, index + 1, 0, path, paths);
+            path.pop_back();
+        }
+        if (skipKaro >= BuyKaro)
+        {
+            getTransactionPath(dp, prices, index + 1, 1, path, paths);
+        }
+    }
+    else
+    {
+        int sellKaro = prices[index] * K + dp[index + 1][1];
+        int skipKaro = 0 + dp[index + 1][0];
+        if (sellKaro >= skipKaro)
+        {
+            path.push_back(index);
+            getTransactionPath(dp, prices, index + 1, 1, path, paths);
+            path.pop_back();
+        }
+        if (skipKaro >= sellKaro)
+        {
+            getTransactionPath(dp, prices, index + 1, 0, path, paths);
+        }
+    }
+}
+
+vector<vector<int>> solvetab(vector<int> &prices)
 {
     int n = prices.size();
 
@@ -31,7 +71,20 @@ int solvetab(vector<int> &prices)
             dp[index][buy] = profit;
         }
     }
-    return dp[0][1];
+    vector<vector<int>> paths;
+    vector<int> path;
+    /*    getTransactionPath(dp, prices, 0, 1, path, paths);
+       for (auto &path : paths)
+       {
+           cout << "Transaction path: ";
+           for (auto &index : path)
+           {
+               cout << index << " ";
+           }
+           cout << endl;
+       } */
+
+    return dp;
 }
 
 int main()
@@ -50,8 +103,59 @@ int main()
         {
             cin >> prices[i];
         }
+        vector<vector<int>> dp(D + 1, vector<int>(2, 0));
+        dp = solvetab(prices);
+        if (tipo == 1)
+        {
+            printf("%d\n", dp[0][1]);
+        }
+        if (tipo == 2)
+        {
+            vector<vector<int>> paths;
+            vector<int> path;
+            getTransactionPath(dp, prices, 0, 1, path, paths);
+            vector<int> v = paths[0];
+            int contador = 0;
+            int valor = 0;
+            path = vector<int>(D, 0);
+            printf("%d\n", dp[0][1]);
+            for (int i = 0; i < v.size(); i++)
+            {
 
-        printf("%d\n", solvetab(prices));
+                if (contador == 0)
+                {
+                    path[v[i]] = K;
+                    contador++;
+                }
+                else
+                {
+                    path[v[i]] = -K;
+                    contador = 0;
+                }
+            }
+            for (int i = 0; i < path.size(); i++)
+            {
+                if (i == path.size() - 1)
+                {
+                    printf("%d\n", path[i]);
+                }
+                else
+                {
+                    printf("%d ", path[i]);
+                }
+            }
+        }
+        if (tipo == 3)
+        {
+            vector<vector<int>> paths;
+            vector<int> path;
+            getTransactionPath(dp, prices, 0, 1, path, paths);
+            vector<int> v = paths[0];
+            int contador = 0;
+            int valor = 0;
+            path = vector<int>(D, 0);
+            printf("%d %lu\n", dp[0][1], paths.size());
+        }
     }
 
     return 0;
